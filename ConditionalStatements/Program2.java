68c48e5545d766535bc4387bb811a1ab538a1c25
Prabhaharanks <89632714+Prabhaharanks@users.noreply.github.com>
2023-02-06 20:25:28 +0530

    Create Program2.java

---
+++ ConditionalStatements/Program2.java
@@ -0,0 +1,21 @@
+package java;
+import java.util.*;
+public class program2 {
+
+public static void main(String[] args) {
+Scanner sc= new Scanner(System.in);
+System.out.print("Enter number1 :");
+int inp1=sc.nextInt();
+System.out.print("Enter number2 :");
+int inp2=sc.nextInt();
+sc.close();
+if(inp1<inp2) {
+System.out.println(inp2+" is greater than "+inp1);
+}
+else {
+System.out.println(inp1+" is greater than "+inp2);
+}
+
+}
+
+}
