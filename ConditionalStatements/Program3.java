6086397872da0340bffcd195136308bc001fcd05
Prabhaharanks <89632714+Prabhaharanks@users.noreply.github.com>
2023-02-06 20:27:28 +0530

    Create Program3.java

---
+++ ConditionalStatements/Program3.java
@@ -0,0 +1,19 @@
+package java;
+import java.util.*;
+
+public class program3 {
+
+public static void main(String[] args) {
+System.out.print("Enter number :");
+Scanner sc= new Scanner(System.in);
+int inp=sc.nextInt();
+if(inp%2==0) {
+System.out.println("EVEN");
+}
+else {
+System.out.println("ODD");
+}
+
+}
+
+}
