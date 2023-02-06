fa515a9110f9c4cca6b22dc08838753057b5bfda
Prabhaharanks <89632714+Prabhaharanks@users.noreply.github.com>
2023-02-06 20:22:57 +0530

    Create Program1.java

---
+++ ConditionalStatements/Program1.java
@@ -0,0 +1,18 @@
+package java;
+import java.util.*;
+public class Program1 {
+
+public static void main(String[] args) {
+System.out.print("Enter number :");
+Scanner sc= new Scanner(System.in);
+int inp=sc.nextInt();
+if(inp<0) {
+System.out.println("Negative");
+}
+else {
+System.out.println("Positive");
+}
+
+}
+
+}
