039c022da626e12f69e244fcd514f4e1b562221b
Prabhaharanks <89632714+Prabhaharanks@users.noreply.github.com>
2023-02-07 21:53:06 +0530

    Create Program1.java

---
+++ DAY2/Program1.java
@@ -0,0 +1,21 @@
+import java.io.*;
+
+import java.util.*;
+
+// Read only region start
+
+class Isodd
+
+{
+
+public int isOdd(int input1){
+
+if(input1%2!=0) return 2;
+
+else
+
+return 1;
+
+}
+
+}
